8272e4a544987f680c86adcc0cb5938888907072
Ghost3298 <eddy.lebbos@hotmail.com>
2024-09-27 15:00:36 +0300

    back to the begining

--- public/php/conn.php
+++ public/php/conn.php
@@ -1,25 +1,18 @@
 <?php
+$host = '127.20.10.2';
+$db = 'lawdata'; 
+$user = 'root'; 
+$pass = ''; 
 
-header("Access-Control-Allow-Origin: *"); // Allows all origins
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Allows specific HTTP methods
-header("Access-Control-Allow-Headers: Content-Type"); // Allows specific headers
-
-// Handle preflight requests
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit;
-}
-
-// Your database connection code
-$host = 'sql308.infinityfree.com';
-$db = 'if0_37397223_lawdata'; 
-$user = 'if0_37397223'; 
-$pass = 'seAe0kdOF7JHa3'; 
-
-mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+
+
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
+header("Access-Control-Allow-Headers: Content-Type"); 
+
 ?>
