c6d9255c796cbc39830f0b4863ec6175ee495fa8
Ghost3298 <eddy.lebbos@hotmail.com>
2024-09-27 13:37:28 +0300

    conn change

--- public/php/conn.php
+++ public/php/conn.php
@@ -1,8 +1,8 @@
 <?php
-$host = '127.20.10.2';
-$db = 'lawdata'; 
-$user = 'root'; 
-$pass = ''; 
+$host = 'sql308.byetcluster.com';
+$db = 'if0_37397223_lawdata'; 
+$user = 'if0_37397223'; 
+$pass = 'seAe0kdOF7JHa3'; // Removed trailing space
 
 $conn = new mysqli($host, $user, $pass, $db);
 
@@ -10,9 +10,7 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-
-header("Access-Control-Allow-Origin: http://localhost:3000");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); 
-header("Access-Control-Allow-Headers: Content-Type"); 
-
+header("Access-Control-Allow-Origin: *"); // Allow access from any origin
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS"); // Allow specific HTTP methods
+header("Access-Control-Allow-Headers: Content-Type, Authorization")
 ?>
